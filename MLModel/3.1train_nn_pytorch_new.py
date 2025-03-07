75db7505ee8bad442bf235e91f97900842c9e68e
吴卓航（台式机） <3224516820@qq.com>
2025-03-07 13:51:06 +0000

    #0.3.7 Use LeakyRelu and more layers

--- MLModel/3.1train_nn_pytorch_new.py
+++ MLModel/3.1train_nn_pytorch_new.py
@@ -6,13 +6,22 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import StandardScaler
+import joblib
 
 # Load dataset
-data = pd.read_csv('2.7cos_transformed.csv', skiprows=1)  # Skip the first row (header)
+data = pd.read_csv('2.7sin_cos_transformed - Copy.csv', skiprows=1)  # Skip the first row (header)
 
 # Prepare input and output data
-X = data.iloc[:, [3, 4, 5, 6, 7, 9, 10]].values  # Columns 4, 5, 6, 7, 8, 10, 11 as input
+X = data.iloc[:, [i for i in range(3, 48) if i != 8]].values  # Columns 4, 5, 6, 7, 8, 10, 11 as input
 y = data.iloc[:, 8].values    # Column 9 as output
+
+# Normalize input data
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# Save the scaler
+joblib.dump(scaler, 'scaler.pkl')
 
 X = X.astype(np.float32)
 y = y.astype(np.float32)
@@ -38,28 +47,31 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(7, 64)  # Adjust input size to 7
-        self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(64, 32)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(32, 1)
-    
+        self.fc1 = nn.Linear(44, 128)  # Adjust input size to 7
+        self.dropout1 = nn.Dropout(0.25)
+        self.fc2 = nn.Linear(128, 64)
+        self.dropout2 = nn.Dropout(0.25)
+        self.fc3 = nn.Linear(64, 32)
+        self.dropout3 = nn.Dropout(0.25)
+        self.fc4 = nn.Linear(32, 1) 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = torch.nn.functional.leaky_relu(self.fc1(x), negative_slope=0.01)
+        x = torch.nn.functional.leaky_relu(self.fc2(x), negative_slope=0.01)
+        x = torch.nn.functional.leaky_relu(self.fc3(x), negative_slope=0.01)
+        x = self.fc4(x)
         return x
 
 model = NeuralNetwork()
 
 # Define loss function and optimizer
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+criterion = nn.HuberLoss()
+# L2: weight_decay
+optimizer = optim.Adam(model.parameters(), weight_decay=0.0001, lr=0.0001)
 
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=400, gamma=0.8)
 
 # Train the model
-num_epochs = 2000
+num_epochs = 1200
 train_losses = []
 val_losses = []
 
